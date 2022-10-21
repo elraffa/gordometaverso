712b3fad87e51576fcab1910bd69b2fec13d4784
elraffa <federico@wcanvas.com>
2022-10-21 14:24:43 -0300

    add scss support

--- components/header.js
+++ components/header.js
@@ -1,10 +1,19 @@
+import '../styles/Header.module.scss';
+
 export default function Header() {
   return (
-    <div className="flex min-h-screen items-center justify-center text-white flex-col bg-cover bg-top bg-no-repeat bg-hero-pattern">
-      <nav></nav>
-      <h1 className="text-4xl md:text-6xl lg:text-9xl font-bold">
-        Gordometaverso
-      </h1>
-    </div>
+    <>
+      <div className="header__topbar">Gordometaverso</div>
+      <div className="header flex min-h-screen items-center justify-center text-white flex-col bg-cover bg-top bg-no-repeat bg-hero-pattern">
+        <nav className="header__navbar">
+          <a href="">Sobre Mi</a>
+          <a href="">Shows</a>
+          <a href="">Eventos</a>
+          <a href="">Videos</a>
+          <a href="">Contacto</a>
+        </nav>
+      </div>
+    </>
+
   );
 }
