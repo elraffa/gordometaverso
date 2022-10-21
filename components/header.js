015548ce248312bab35ccbd39539329e51b304f5
elraffa <federico@wcanvas.com>
2022-10-21 19:09:16 -0300

    add active link style;

--- components/header.js
+++ components/header.js
@@ -1,17 +1,23 @@
 import '../styles/Header.module.scss';
 import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from "next/router";
+
 
 export default function Header() {
+  const isActive = useState(false)
+  const router = useRouter();
+
   return (
     <>
       <div className="header__topbar">Gordometaverso</div>
       <div className="header bg-cover bg-top bg-no-repeat bg-hero-pattern">
         <nav className="header__navbar">
-          <Link href='/#title'><a className='header__links'>Sobre Mi</a></Link>
-          <Link href='/shows#title'><a className="header__links">Shows</a></Link>
-          <Link href='/eventos'><a className="header__links" href="">Eventos</a></Link>
-          <Link href='/videos'><a className="header__links" href="">Videos</a></Link>
-          <Link href='/contacto'><a className="header__links" href="">Contacto</a></Link>
+          <Link href='/#title'><a className={router.pathname == "/" ? 'header__links--active' : 'header__links'}>Sobre Mi</a></Link>
+          <Link href='/shows#title'><a className={router.pathname == "/shows" ? 'header__links--active' : 'header__links'}>Shows</a></Link>
+          <Link href='/eventos'><a className={router.pathname == "/eventos" ? 'header__links--active' : 'header__links'} href="">Eventos</a></Link>
+          <Link href='/videos'><a className={router.pathname == "/videos" ? 'header__links--active' : 'header__links'}>Videos</a></Link>
+          <Link href='/contacto'><a className={router.pathname == "/contacto" ? 'header__links--active' : 'header__links'}>Contacto</a></Link>
         </nav>
       </div>
     </>
